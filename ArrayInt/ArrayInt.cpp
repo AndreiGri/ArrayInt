b56d58cdb6b59aa87bd41a795ea1971f3ba1bcd4
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-08 20:41:33 +0500

    Добавьте файлы проекта.

---
+++ ArrayInt/ArrayInt.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    const int n = 10;
+    int j = 0, k;
+    cout << "Введите с какого числа начать заполнение массива ";
+    cin >> k;
+    int ar[n];
+    while (j < n) {
+        if (k % 2 != 0) {
+            ar[j] = k;
+            cout << ar[j] << ", ";
+            j++;
+        }
+        k++;
+    }
+    system("pause > nul");
+    return 0;
+}
